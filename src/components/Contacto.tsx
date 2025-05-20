59b9eb7cd960ebd0326971c64974ce058ed54215
Franklin-Gomez <franklin_043097@hotmail.com>
2025-05-20 09:14:32 -0500

    fix image

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -2,7 +2,7 @@
 
 export default function Contacto() {
     return (
-        <section className="bg-[url('img/encuentra.jpg')] bg-center my-8">
+        <section className="bg-[url('/img/encuentra.jpg')] bg-center my-8">
             <div className="container mx-auto flex flex-col items-center justify-center gap-8 py-40 text-white">
                 <h2 className="text-4xl">Encuentra la casa de tus sueños</h2>
                 <p className="text-2xl">Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
