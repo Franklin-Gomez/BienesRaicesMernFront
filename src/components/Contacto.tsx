6e8ab7a5ea6fbff77c6997001c0c2c1cac032bcf
Franklin-Gomez <franklin_043097@hotmail.com>
2025-07-26 21:18:41 -0500

    fix button contacto on index page

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -6,7 +6,7 @@ export default function Contacto() {
             <div className="container mx-auto flex flex-col items-center justify-center gap-8 py-40 text-white">
                 <h2 className="text-4xl">Encuentra la casa de tus sueños</h2>
                 <p className="text-2xl">Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-                <a href="contacto.php" className="uppercase text-white text-center text-xl font-bold w-1/4 bg-orange-400 py-4 rounded hover:bg-orange-500">Contactános</a>
+                <a href="/contacto" className="uppercase text-white text-center text-xl font-bold w-1/4 bg-orange-400 py-4 rounded hover:bg-orange-500">Contactános</a>
             </div>
         </section>
     )
